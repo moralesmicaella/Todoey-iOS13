5cdb2fc2340fe7aaf2e7908fcb65d67aa3e1c95f
Micaella Morales <morales.micaella@gmail.com>
2021-12-24 08:38:19 -0800

    Add search functionality

--- Todoey/Controllers/ToDoListTableViewController.swift
+++ Todoey/Controllers/ToDoListTableViewController.swift
@@ -14,26 +14,32 @@ class ToDoListTableViewController: UITableViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     var toDoItems = [Item]()
+    var filteredToDoItems = [Item]()
+    
+    let blur = UIBlurEffect(style: .extraLight)
+    let blurView = UIVisualEffectView(effect: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadItems()
+        filteredToDoItems = toDoItems
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoItems.count
+        return filteredToDoItems.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
         
-        let item = toDoItems[indexPath.row]
+        let item = filteredToDoItems[indexPath.row]
         cell.textLabel?.text = item.title
         
         cell.accessoryType = item.done ? .checkmark : .none
+        
 
         return cell
     }
@@ -41,7 +47,10 @@ class ToDoListTableViewController: UITableViewController {
     //MARK: - Table view delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        toDoItems[indexPath.row].done = !toDoItems[indexPath.row].done
+        filteredToDoItems[indexPath.row].done = !filteredToDoItems[indexPath.row].done
+        
+        //    context.delete(toDoItems[indexPath.row])
+        //    toDoItems.remove(at: indexPath.row)
         
         self.saveItems()
         
@@ -94,15 +103,41 @@ class ToDoListTableViewController: UITableViewController {
             print("Error fetching data from context \(error)")
         }
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+//MARK: - Search bar delegate
+
+extension ToDoListTableViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredToDoItems = []
+        } else {
+            filteredToDoItems = toDoItems.filter({ (item) in
+                return item.title?.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            })
+        }
+        
+        
+        tableView.reloadData()
     }
-    */
-
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        searchBar.showsCancelButton = true
+        
+        filteredToDoItems = []
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        searchBar.text = ""
+        searchBar.showsCancelButton = false
+        searchBar.resignFirstResponder()
+        
+        filteredToDoItems = toDoItems
+        tableView.reloadData()
+    }
+    
 }
